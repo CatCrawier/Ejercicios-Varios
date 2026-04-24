9164b79447b5259b475184469764848c8f780563
Adrian <adiraprueba@gmail.com>
2026-04-24 08:58:16 -0500

    24/04/2026

---
+++ Python/Notas.py
@@ -0,0 +1,16 @@
+estudiantes = [
+    {
+        "Nombre": "Juan",
+        "Notas": [3.5, 4.0, 3.8]
+    },
+    {
+        "Nombre": "Maria",
+        "Notas": [3.0, 3.5, 3.2]
+    },
+    {
+        "Nombre": "Pedro",
+        "Notas": [3.2, 3.8, 3.6]
+    },
+]
+
+print(f"Notas de los estudiantes: {len(estudiantes)}")
