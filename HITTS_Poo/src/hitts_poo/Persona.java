94095151bbf09b54f6a166e07705d9ab1a5c7489
Camilo Hernandez <hgcamilo94@gmail.com>
2026-06-05 20:14:32 -0500

    05/06/2026

--- HITTS_Poo/src/hitts_poo/Persona.java
+++ HITTS_Poo/src/hitts_poo/Persona.java
@@ -1,24 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package hitts_poo;
 
-/**
- *
- * @author Cesde
- */
 public class Persona {
 
-    String nombre;
-    String apellido;
+    private String nombre;
+    private String apellido;
     int edad;
-    char sexo;
+    private char sexo;
 
     public Persona(String nombre, String apellido, int edad, char sexo) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.edad = edad;
+        this.sexo = sexo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }   
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setSexo(char sexo) {
         this.sexo = sexo;
     }
 
@@ -28,5 +52,4 @@ public class Persona {
         System.out.println("Edad: " + edad);
         System.out.println("Sexo: " + sexo);
     }
-    
 }
